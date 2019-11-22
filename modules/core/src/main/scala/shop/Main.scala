025bbc74cce7ee3d52a443e647ab87fa095794ea
Gabriel Volpe <volpegabriel@gmail.com>
2019-11-22 18:24:03 -0300

    simpler main

--- modules/core/src/main/scala/shop/Main.scala
+++ modules/core/src/main/scala/shop/Main.scala
@@ -1,48 +1,37 @@
 package shop
 
-import cats.Parallel
 import cats.effect._
 import cats.implicits._
 import io.chrisdavenport.log4cats.Logger
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
-import modules._
 import org.http4s.server.blaze.BlazeServerBuilder
-import shop.config.data.AppConfig
+import shop.modules._
 
 object Main extends IOApp {
 
-  implicit val logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+  implicit val logger = Slf4jLogger.getLogger[IO]
 
   override def run(args: List[String]): IO[ExitCode] =
     config.load[IO].flatMap { cfg =>
       Logger[IO].info(s"Loaded config $cfg") *>
         AppResources.make[IO](cfg).use { res =>
-          Server.httpApi[IO](cfg, res).flatMap { api =>
-            val sc = cfg.httpServerConfig
-            BlazeServerBuilder[IO]
-              .bindHttp(sc.port.value, sc.host.value)
-              .withHttpApp(api.httpApp)
-              .serve
-              .compile
-              .drain
-              .as(ExitCode.Success)
-          }
+          for {
+            security <- Security.make[IO](cfg, res.psql, res.redis)
+            algebras <- Algebras.make[IO](res.redis, res.psql, cfg.cartExpiration)
+            clients <- HttpClients.make[IO](cfg.paymentConfig, res.client)
+            programs <- Programs.make[IO](cfg.checkoutConfig, algebras, clients)
+            api <- HttpApi.make[IO](algebras, programs, security)
+            _ <- BlazeServerBuilder[IO]
+                  .bindHttp(
+                    cfg.httpServerConfig.port.value,
+                    cfg.httpServerConfig.host.value
+                  )
+                  .withHttpApp(api.httpApp)
+                  .serve
+                  .compile
+                  .drain
+          } yield ExitCode.Success
         }
     }
-}
-
-object Server {
-
-  def httpApi[F[_]: Concurrent: ContextShift: Logger: Parallel: Timer](
-      cfg: AppConfig,
-      res: AppResources[F]
-  ): F[HttpApi[F]] =
-    for {
-      security <- Security.make[F](cfg, res.psql, res.redis)
-      algebras <- Algebras.make[F](res.redis, res.psql, cfg.cartExpiration)
-      clients <- HttpClients.make[F](cfg.paymentConfig, res.client)
-      programs <- Programs.make[F](cfg.checkoutConfig, algebras, clients)
-      api <- HttpApi.make[F](algebras, programs, security)
-    } yield api
 
 }
