52dd5168c74a73f9e3be217fad0960d9a82687f4
Jakub Kozłowski <kubukoz@gmail.com>
2019-11-22 02:08:48 +0100

    Alternative implementation of passing resources to tests

--- modules/tests/src/main/scala/suite/resourcesuite.scala
+++ modules/tests/src/main/scala/suite/resourcesuite.scala
@@ -4,6 +4,7 @@ import cats.effect._
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.compatible.Assertion
 import org.scalactic.source.Position
+import cats.effect.concurrent.Deferred
 
 trait ResourceSuite[A] extends PureTestSuite with BeforeAndAfterAll {
 
@@ -12,11 +13,14 @@ trait ResourceSuite[A] extends PureTestSuite with BeforeAndAfterAll {
   private[this] var res: A            = _
   private[this] var cleanUp: IO[Unit] = _
 
+  private[this] val latch = Deferred[IO, Unit].unsafeRunSync()
+
   override def beforeAll(): Unit = {
     super.beforeAll()
     val (r, h) = resources.allocated.unsafeRunSync()
     res = r
     cleanUp = h
+    latch.complete(()).unsafeRunSync()
   }
 
   override def afterAll(): Unit = {
@@ -24,7 +28,10 @@ trait ResourceSuite[A] extends PureTestSuite with BeforeAndAfterAll {
     super.afterAll()
   }
 
-  def specR(testName: String)(f: A => IO[Assertion])(implicit pos: Position): Unit =
-    spec(testName)(f(res))
-
+  //if only this by-name could go away, I'd be much happier
+  def withResources(f: (=> A) => Unit): Unit = f {
+    //just to ensure that the resource has actually been allocated (although it should have been, right?)
+    latch.get.unsafeRunSync
+    res
+  }
 }
