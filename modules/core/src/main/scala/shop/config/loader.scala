de76ae43c118ab67ffc0b85f6d39a507d1f4b2c7
Viktor Lovgren <viktor@lovg.ren>
2019-12-05 21:30:56 +0000

    Change to only use parMapN for configuration

--- modules/core/src/main/scala/shop/config/loader.scala
+++ modules/core/src/main/scala/shop/config/loader.scala
@@ -13,68 +13,57 @@ import scala.concurrent.duration._
 import shop.config.data._
 
 object load {
-
   // Ciris promotes configuration as code
-  def apply[F[_]: Async: ContextShift]: F[AppConfig] =
-    env("SC_APP_ENV")
-      .as[AppEnvironment]
-      .flatMap {
-        case Test =>
-          default(
-            redisUri = RedisURI("redis://localhost"),
-            paymentUri = PaymentURI("http://10.123.154.10/api")
+  def apply[F[_]: Async: ContextShift]: F[AppConfig] = {
+    val config =
+      (
+        env("SC_APP_ENV").as[AppEnvironment],
+        env("SC_JWT_SECRET_KEY").as[NonEmptyString].secret,
+        env("SC_JWT_CLAIM").as[NonEmptyString].secret,
+        env("SC_ACCESS_TOKEN_SECRET_KEY").as[NonEmptyString].secret,
+        env("SC_ADMIN_USER_TOKEN").as[NonEmptyString].secret,
+        env("SC_PASSWORD_SALT").as[NonEmptyString].secret
+      ).parMapN { (environment, secretKey, claimStr, tokenKey, adminToken, salt) =>
+        AppConfig(
+          AdminJwtConfig(
+            JwtSecretKeyConfig(secretKey),
+            JwtClaimConfig(claimStr),
+            AdminUserTokenConfig(adminToken)
+          ),
+          JwtSecretKeyConfig(tokenKey),
+          PasswordSalt(salt),
+          TokenExpiration(30.minutes),
+          ShoppingCartExpiration(30.minutes),
+          CheckoutConfig(
+            retriesLimit = 3,
+            retriesBackoff = 10.milliseconds
+          ),
+          PaymentConfig(PaymentURI(environment match {
+            case Test => "http://10.123.154.10/api"
+            case Prod => "https://payments.net/api"
+          })),
+          HttpClientConfig(
+            connectTimeout = 2.seconds,
+            requestTimeout = 2.seconds
+          ),
+          PostgreSQLConfig(
+            host = "localhost",
+            port = 5432,
+            user = "postgres",
+            database = "store",
+            max = 10
+          ),
+          RedisConfig(RedisURI(environment match {
+            case Test => "redis://localhost"
+            case Prod => "redis://10.123.154.176"
+          })),
+          HttpServerConfig(
+            host = "0.0.0.0",
+            port = 8080
           )
-        case Prod =>
-          default(
-            redisUri = RedisURI("redis://10.123.154.176"),
-            paymentUri = PaymentURI("https://payments.net/api")
-          )
-      }
-      .load[F]
-
-  private def default(
-      redisUri: RedisURI,
-      paymentUri: PaymentURI
-  ): ConfigValue[AppConfig] =
-    (
-      env("SC_JWT_SECRET_KEY").as[NonEmptyString].secret,
-      env("SC_JWT_CLAIM").as[NonEmptyString].secret,
-      env("SC_ACCESS_TOKEN_SECRET_KEY").as[NonEmptyString].secret,
-      env("SC_ADMIN_USER_TOKEN").as[NonEmptyString].secret,
-      env("SC_PASSWORD_SALT").as[NonEmptyString].secret
-    ).parMapN { (secretKey, claimStr, tokenKey, adminToken, salt) =>
-      AppConfig(
-        AdminJwtConfig(
-          JwtSecretKeyConfig(secretKey),
-          JwtClaimConfig(claimStr),
-          AdminUserTokenConfig(adminToken)
-        ),
-        JwtSecretKeyConfig(tokenKey),
-        PasswordSalt(salt),
-        TokenExpiration(30.minutes),
-        ShoppingCartExpiration(30.minutes),
-        CheckoutConfig(
-          retriesLimit = 3,
-          retriesBackoff = 10.milliseconds
-        ),
-        PaymentConfig(paymentUri),
-        HttpClientConfig(
-          connectTimeout = 2.seconds,
-          requestTimeout = 2.seconds
-        ),
-        PostgreSQLConfig(
-          host = "localhost",
-          port = 5432,
-          user = "postgres",
-          database = "store",
-          max = 10
-        ),
-        RedisConfig(redisUri),
-        HttpServerConfig(
-          host = "0.0.0.0",
-          port = 8080
         )
-      )
-    }
+      }
 
+    config.load[F]
+  }
 }
