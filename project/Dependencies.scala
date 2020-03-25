4ed1e6cee6a97c5f1bb63b5bf748285f4aa38a53
Scala Steward <me@scala-steward.org>
2020-03-25 04:20:12 +0100

    Update http4s-blaze-client, ... to 0.21.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val ciris         = "1.0.4"
     val javaxCrypto   = "1.0.1"
     val fs2           = "2.3.0"
-    val http4s        = "0.21.1"
+    val http4s        = "0.21.2"
     val http4sJwtAuth = "0.0.4"
     val log4cats      = "1.0.1"
     val newtype       = "0.4.3"
