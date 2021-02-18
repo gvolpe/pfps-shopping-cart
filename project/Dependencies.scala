1b44a03c2c24320556f9a957f172e0642e8d982e
Scala Steward <me@scala-steward.org>
2021-02-18 21:07:41 +0100

    Update fs2-core to 2.5.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val ciris         = "1.2.1"
     val derevo        = "0.12.0"
     val javaxCrypto   = "1.0.1"
-    val fs2           = "2.5.1"
+    val fs2           = "2.5.2"
     val http4s        = "0.21.19"
     val http4sJwtAuth = "0.0.5"
     val log4cats      = "1.1.1"
