891bbd101dd05d380459cc14a03e498c4c81c64c
Gabriel Volpe <volpegabriel@gmail.com>
2021-02-28 18:31:16 +0100

    format file

--- modules/core/src/main/scala/shop/effects/Background.scala
+++ modules/core/src/main/scala/shop/effects/Background.scala
@@ -7,25 +7,15 @@ import cats.effect.implicits._
 import cats.syntax.all._
 
 trait Background[F[_]] {
-  def schedule[A](
-      fa: F[A],
-      duration: FiniteDuration
-  ): F[Unit]
+  def schedule[A](fa: F[A], duration: FiniteDuration): F[Unit]
 }
 
-// TODO: Replace for the new builtin background based on Resource
 object Background {
   def apply[F[_]](implicit ev: Background[F]): Background[F] = ev
 
   implicit def concurrentBackground[F[_]: Concurrent: Timer]: Background[F] =
     new Background[F] {
-
-      def schedule[A](
-          fa: F[A],
-          duration: FiniteDuration
-      ): F[Unit] =
+      def schedule[A](fa: F[A], duration: FiniteDuration): F[Unit] =
         (Timer[F].sleep(duration) *> fa).start.void
-
     }
-
 }
