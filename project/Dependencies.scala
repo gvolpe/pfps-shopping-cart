c9a9710e7bad95b9a2da5e57fcbaed6a6dae96ac
Scala Steward <me@scala-steward.org>
2021-03-05 22:22:55 +0100

    Update redis4cats-effects, ... to 0.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val log4cats      = "1.1.1"
     val newtype       = "0.4.4"
     val refined       = "0.9.21"
-    val redis4cats    = "0.11.1"
+    val redis4cats    = "0.12.0"
     val skunk         = "0.0.24"
     val squants       = "1.7.0"
 
