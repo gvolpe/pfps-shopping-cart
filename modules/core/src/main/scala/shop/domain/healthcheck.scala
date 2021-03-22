5d3067039494423f74a206629b64e198e07a1569
Gabriel Volpe <volpegabriel@gmail.com>
2021-03-22 14:49:59 +0100

    HealthCheck: Add Status datatype

--- modules/core/src/main/scala/shop/domain/healthcheck.scala
+++ modules/core/src/main/scala/shop/domain/healthcheck.scala
@@ -1,19 +1,37 @@
 package shop.domain
 
-import derevo.circe.magnolia.{ decoder, encoder }
+import shop.optics.Iso
+
+import derevo.circe.magnolia.encoder
 import derevo.derive
+import io.circe.Encoder
 import io.estatico.newtype.macros._
 
 object healthcheck {
-  @derive(decoder, encoder)
-  @newtype
-  case class RedisStatus(value: Boolean)
+  sealed trait Status
+  object Status {
+    case object Okay extends Status
+    case object Unreachable extends Status
 
-  @derive(decoder, encoder)
-  @newtype
-  case class PostgresStatus(value: Boolean)
+    val _Bool: Iso[Boolean, Status] =
+      Iso(b => if (b) Okay else Unreachable, {
+        case Okay        => true
+        case Unreachable => false
+      })
 
-  @derive(decoder, encoder)
+    implicit val jsonEncoder: Encoder[Status] =
+      Encoder.forProduct1("status")(_.toString)
+  }
+
+  @derive(encoder)
+  @newtype
+  case class RedisStatus(value: Status)
+
+  @derive(encoder)
+  @newtype
+  case class PostgresStatus(value: Status)
+
+  @derive(encoder)
   case class AppStatus(
       redis: RedisStatus,
       postgres: PostgresStatus
