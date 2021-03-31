15a8d2e27e0c5e082ed4016f1d70ade4ff113d45
Gabriel Volpe <volpegabriel@gmail.com>
2021-03-31 15:45:17 +0200

    Implement Background in terms of Supervisor

--- modules/core/src/main/scala/shop/Main.scala
+++ modules/core/src/main/scala/shop/Main.scala
@@ -3,6 +3,7 @@ package shop
 import shop.modules._
 
 import cats.effect._
+import cats.effect.std.Supervisor
 import eu.timepit.refined.auto._
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.Server
@@ -20,27 +21,29 @@ object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] =
     config.load[IO].flatMap { cfg =>
       Logger[IO].info(s"Loaded config $cfg") >>
-        AppResources
-          .make[IO](cfg)
-          .evalMap { res =>
-            Security.make[IO](cfg, res.psql, res.redis).map { security =>
-              val clients  = HttpClients.make[IO](cfg.paymentConfig, res.client)
-              val services = Services.make[IO](res.redis, res.psql, cfg.cartExpiration)
-              val programs = Programs.make[IO](cfg.checkoutConfig, services, clients)
-              val api      = HttpApi.make[IO](services, programs, security)
-              cfg.httpServerConfig -> api
+        Supervisor[IO].use { implicit sp =>
+          AppResources
+            .make[IO](cfg)
+            .evalMap { res =>
+              Security.make[IO](cfg, res.psql, res.redis).map { security =>
+                val clients  = HttpClients.make[IO](cfg.paymentConfig, res.client)
+                val services = Services.make[IO](res.redis, res.psql, cfg.cartExpiration)
+                val programs = Programs.make[IO](cfg.checkoutConfig, services, clients)
+                val api      = HttpApi.make[IO](services, programs, security)
+                cfg.httpServerConfig -> api
+              }
             }
-          }
-          .flatMap {
-            case (cfg, api) =>
-              EmberServerBuilder
-                .default[IO]
-                .withHost(cfg.host)
-                .withPort(cfg.port)
-                .withHttpApp(api.httpApp)
-                .build
-          }
-          .use(showEmberBanner(_) >> IO.never.as(ExitCode.Success))
+            .flatMap {
+              case (cfg, api) =>
+                EmberServerBuilder
+                  .default[IO]
+                  .withHost(cfg.host)
+                  .withPort(cfg.port)
+                  .withHttpApp(api.httpApp)
+                  .build
+            }
+            .use(showEmberBanner(_) >> IO.never.as(ExitCode.Success))
+        }
     }
 
 }
--- modules/core/src/main/scala/shop/effects/Background.scala
+++ modules/core/src/main/scala/shop/effects/Background.scala
@@ -3,7 +3,7 @@ package shop.effects
 import scala.concurrent.duration.FiniteDuration
 
 import cats.effect._
-import cats.effect.implicits._
+import cats.effect.std.Supervisor
 import cats.syntax.all._
 
 trait Background[F[_]] {
@@ -13,9 +13,9 @@ trait Background[F[_]] {
 object Background {
   def apply[F[_]: Background]: Background[F] = implicitly
 
-  implicit def concurrentBackground[F[_]: Temporal]: Background[F] =
+  implicit def bgInstance[F[_]](implicit S: Supervisor[F], T: Temporal[F]): Background[F] =
     new Background[F] {
       def schedule[A](fa: F[A], duration: FiniteDuration): F[Unit] =
-        (Temporal[F].sleep(duration) *> fa).start.void
+        S.supervise(T.sleep(duration) *> fa).void
     }
 }
