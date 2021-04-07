d88940b90f1742e31bafcb29191c0ff95fb7a593
Gabriel Volpe <volpegabriel@gmail.com>
2021-04-07 12:27:45 +0200

    Improve keyEncoder and keyDecoder impl

--- modules/core/src/main/scala/shop/ext/circe/keyEncoder.scala
+++ modules/core/src/main/scala/shop/ext/circe/keyEncoder.scala
@@ -2,18 +2,21 @@ package shop.ext.circe
 
 import derevo.{ Derivation, NewTypeDerivation }
 import io.circe.KeyEncoder
-import magnolia.{ CaseClass, Magnolia }
+import magnolia.{CaseClass, Magnolia, SealedTrait}
 
-object keyEncoder extends Derivation[KeyEncoder] with NewTypeDerivation[KeyEncoder] {
+class keyEncoder(sep: String = "::") {
   type Typeclass[T] = KeyEncoder[T]
 
-  def combine[T](ctx: CaseClass[KeyEncoder, T]): KeyEncoder[T] = new KeyEncoder[T] {
-    def apply(key: T): String =
-      ctx.parameters.toList match {
-        case (p :: _) => p.typeclass.apply(key.asInstanceOf[p.PType])
-        case _        => "error"
-      }
-  }
+  def combine[T](ctx: CaseClass[KeyEncoder, T]): KeyEncoder[T] =
+    if (ctx.isObject) _ => ctx.typeName.short
+    else { cc =>
+      ctx.parameters.view.map(p => p.typeclass(p.dereference(cc))).mkString(sep)
+    }
+
+  def dispatch[T](ctx: SealedTrait[KeyEncoder, T]): KeyEncoder[T] =
+    obj => ctx.dispatch(obj)(sub => sub.typeclass(sub.cast(obj)))
 
   def instance[T]: KeyEncoder[T] = macro Magnolia.gen[T]
 }
+
+object keyEncoder extends keyEncoder("::") with Derivation[KeyEncoder] with NewTypeDerivation[KeyEncoder]
