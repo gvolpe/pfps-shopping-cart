13d2082668e4a6a8ed1dfc0a14a1cf144648ac6f
Scala Steward <me@scala-steward.org>
2021-07-13 16:34:27 +0200

    Update semanticdb-scalac to 4.4.24

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val kindProjector    = "0.13.0"
     val logback          = "1.2.3"
     val organizeImports  = "0.5.0"
-    val semanticDB       = "4.4.23"
+    val semanticDB       = "4.4.24"
 
     val weaver = "0.7.4"
   }
