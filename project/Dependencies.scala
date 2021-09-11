21864bc48bf30ea43825ca6301a5ae2d90af61b0
Scala Steward <me@scala-steward.org>
2021-09-11 04:37:55 +0200

    Update http4s-circe, http4s-dsl, ... to 0.23.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val derevo        = "0.12.6"
     val javaxCrypto   = "1.0.1"
     val fs2           = "3.1.2"
-    val http4s        = "0.23.1"
+    val http4s        = "0.23.3"
     val http4sJwtAuth = "1.0.0"
     val log4cats      = "2.1.1"
     val monocle       = "3.1.0"
